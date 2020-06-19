29a733e32744578dc800473cb4c7d3697c104754
Adrian Javier <y0aj24@gmail.com>
2020-06-18 20:22:41 -0500

    Updates

---
+++ precache-manifest.1b4ec20a3872e2a9fcb280215e10599b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8d084d61371cbfab8382b058cda5ed8",
+    "url": "/react-app-phone-store/index.html"
+  },
+  {
+    "revision": "ef3a7f3081ecfc43a6b1",
+    "url": "/react-app-phone-store/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "24be425299c8627f019c",
+    "url": "/react-app-phone-store/static/css/main.f69672f6.chunk.css"
+  },
+  {
+    "revision": "ef3a7f3081ecfc43a6b1",
+    "url": "/react-app-phone-store/static/js/2.7beada60.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app-phone-store/static/js/2.7beada60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24be425299c8627f019c",
+    "url": "/react-app-phone-store/static/js/main.7630d7c5.chunk.js"
+  },
+  {
+    "revision": "124724b9be9e10237e98",
+    "url": "/react-app-phone-store/static/js/runtime-main.ac79890c.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/react-app-phone-store/static/media/logo.9f7e506d.svg"
+  }
+]);
